ebc631662ee23b7ea4e80cbcae1dc5bbe663b084
Himanshu-1994 <angad1994@gmail.com>
2022-02-14 19:12:08 -0600

    zip

--- assignment1/setup.py
+++ assignment1/setup.py
@@ -16,9 +16,17 @@ status = import_or_install(package)
 try:
     import wget
 except ImportError:
-    print("wget not installed")
+    print("Package 'wget' does not exist. Please install")
     sys.exit()
 
 url = "http://nlp.stanford.edu/data/glove.6B.zip"
 filename = wget.download(url)
 print(filename)
+
+try:
+    from zipfile import ZipFile
+except ImportError:
+    print("Package 'zipfile' does not exist. Please install")
+    sys.exit()
+
+ZipFile("filename").extractall(".")
