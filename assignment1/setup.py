1e82f2d88bbaa2499de02d54519f25b94b2065c2
Himanshu-1994 <angad1994@gmail.com>
2022-02-14 18:25:31 -0600

    update

--- assignment1/setup.py
+++ assignment1/setup.py
@@ -1,12 +1,23 @@
 import pip
-
+import sys
 
 #Reference https://stackoverflow.com/questions/4527554/check-if-module-exists-if-not-install-it
 
 def import_or_install(package):
     try:
         __import__(package)
+        print("successfully imported - ",package)
+        return 1
     except ImportError:
         pip.main(['install', package])   
-    
-import_or_install("wget")
+    return 0
+
+package = "wget"
+status = import_or_install(package)
+
+if not status:
+    try:
+        __import__(package)
+    except ImportError:
+        print("Package: ",package, "not installed")
+        sys.exit()
